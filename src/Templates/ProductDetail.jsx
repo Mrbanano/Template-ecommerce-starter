0bdf8f6f0e269423f3e84ee3f52450a105e65cd5
Alvaro castillo <6666alvaro666@gmail.com>
2021-06-23 19:28:00 -0500

    create skeleton of product detail +

--- src/Templates/ProductDetail.jsx
+++ src/Templates/ProductDetail.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import { graphql } from 'gatsby'
 
-export default function ProductDetail() {
+import Layout from '../components/Layout'
+
+export default function ProductDetail(props) {
+     const ProductDetail = props.data.stripeProduct
+
+     const image = ProductDetail.images[0]
     return (
         <div>
-            work
+            <Layout>
+            <p>{ProductDetail.name}</p>
+            <p>{ProductDetail.description}</p>
+            <img src={image} alt="" />
+            </Layout>
         </div>
     )
 }
+
+export const pageQuery = graphql`
+query($id : String) {
+    stripeProduct(id: {eq: $id}) {
+      description
+      images
+      name
+    }
+  }
+`;
