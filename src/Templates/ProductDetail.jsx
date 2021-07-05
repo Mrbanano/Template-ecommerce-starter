2a7f3a8c0c0ab0fb81da41dc1a47adfa2d42faaf
Alvaro castillo <6666alvaro666@gmail.com>
2021-07-05 18:33:19 -0500

    [?]

--- src/Templates/ProductDetail.jsx
+++ src/Templates/ProductDetail.jsx
@@ -1,21 +1,40 @@
 import React from 'react'
-import { graphql } from 'gatsby'
+import { graphql, Link } from 'gatsby'
 
 import Layout from '../components/Layout'
 
 export default function ProductDetail(props) {
-     const ProductDetail = props.data.stripeProduct
+  const ProductDetail = props.data.stripeProduct
 
-     const image = ProductDetail.images[0]
-    return (
-        <div>
-            <Layout>
-            <p>{ProductDetail.name}</p>
-            <p>{ProductDetail.description}</p>
-            <img src={image} alt="" />
-            </Layout>
+  const image = ProductDetail.images[0]
+  return (
+
+    <Layout>
+      <div className="ProductDetail">
+        <div className="ProductDetail-container">
+          <div className="ProductDetail-content">
+            <div className="ProductDetail-image">
+              <img src={image} alt={ProductDetail.name} />
+            </div>
+            <div className="ProductDetail-text">
+              <p className="ProductDetail-title">{ProductDetail.name}</p>
+              <p className="ProductDetail-subtitle">{ProductDetail.description}</p>
+            </div>
+            <div className="ProductDetailFooter">
+              <p><strong> aqui va el selector de cantidad </strong></p>
+              <div className="content-footer-button secundary">
+                <Link className="a" to={"/productos"} >Añadir al carrito </Link>
+              </div>
+              <div className="content-footer-button">
+                <Link className="a" to={"/productos"} >Compar ahora </Link>
+              </div>
+            </div>
+          </div>
         </div>
-    )
+      </div>
+
+    </Layout>
+  )
 }
 
 export const pageQuery = graphql`
